0b698fce451bcc4a9f64ba49a78cd23a4fc94b10
ArlethAndrade <150208591+ArlethAndrade@users.noreply.github.com>
2023-11-19 22:49:23 -0600

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,6 @@
+from turtle import *
+import colorsys
+
+# Agregar el texto en la cabecera
+header_text = "Para Fany...Mi amiga, jamas va a ser espectadora, mientras me tenga a mi <3"
+color("white")  # Color del texto
